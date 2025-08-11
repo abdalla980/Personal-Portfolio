d32303430b61d777de5f8471ce477eee1b615227
abdalla980 <100001276@stud.srh-university.de>
2025-08-11 18:16:20 +0200

    footer edit

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -6,21 +6,31 @@ import styles from './contact.module.css'
 
 export const Contact = () => {
     return (
-       <footer id={"Contact"} className={styles.container}>
-           <div className={styles.text}>
-               <h2>Contact</h2>
-               <p>Feel free to reach out!</p>
-           </div>
-           <ul className={styles.links}>
-               <li className={styles.link}>
-                   <img src={getImageUrl("Email.png")} alt={"Email icon"}/>
-                   <a target='_blank' href={"mailto:abdullahizzldin1@gmail.com"}>Email</a>
-               </li>
-               <li className={styles.link}>
-                   <img src={getImageUrl("githubicon.png")} alt={"GitHub icon"}/>
-                   <a target="_blank" href={"https://github.com/abdalla980"}>GitHub</a>
-               </li>
-           </ul>
-       </footer>
+        <footer id={"Contact"} className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("Email.png")} alt={"Email icon"}/>
+                    <a target='_blank' href={"mailto:abdullahizzldin1@gmail.com"}>Email</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("githubicon.png")} alt={"GitHub icon"}/>
+                    <a target="_blank" href={"https://github.com/abdalla980"}>GitHub</a>
+                </li>
+                <li className={styles.link}>
+                    <img className={styles.LinkedinImg} src={getImageUrl("Linkedin.png")}/>
+                    <a target="_blank" href={"https://www.linkedin.com/in/abdalla-izzeldin-b5721733b/"}>Linkedin</a>
+                </li>
+                <li className={styles.link}>
+                    <img className={styles.Behanceimg} src={getImageUrl("Behance.png")} />
+                    <a target="_blank" href={"https://www.behance.net/AbdallaBabikir"}>Behance</a>
+                </li>
+            </ul>
+
+
+        </footer>
     )
 }
